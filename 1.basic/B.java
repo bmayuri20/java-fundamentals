9ce929de590d8f38f436f5c1911bc92d3f657bdf
bmayuri20 <bmayuri100@gmail.com>
2025-06-01 20:59:58 +0530

    Initial Commit

---
+++ 1.basic/B.java
@@ -0,0 +1,9 @@
+class B{
+
+	public static void main (String[] args)
+	{
+		System.out.println("Name: Mayuri Borkar");
+		System.out.println("College name : DMIETR");
+		System.out.println("Branch: ENTC");
+	}
+}
---
+++ 1.basic/Prime.java
@@ -0,0 +1,24 @@
+public class Prime{
+	public boolean isPrime(int n){
+		if (n == 1)
+            return false;
+		
+		for(int i = 2; i <= n/2; i++){
+			if(n % i == 0)
+				return false;
+		}
+		return true;
+		}
+		
+		public static void main(String[] args){
+			Prime prime = new Prime();
+			
+			if(prime.isPrime(2))
+				System.out.println("Given no is a prime");
+			else
+				System.out.println("Given no is a composite");
+		
+	}
+
+}
+  
